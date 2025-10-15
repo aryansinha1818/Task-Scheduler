d5a1fc8b4366deb78e0f43454bf9a833d3276387
Aryan <aryansinha1818@gmail.com>
2025-10-15 21:51:50 +0530

    Docker

--- backend/config/db.config.js
+++ backend/config/db.config.js
@@ -1,8 +1,23 @@
+// const mongoose = require("mongoose");
+
+// const connectDB = async () => {
+//   await mongoose
+//     .connect(process.env.MONGODB_URL)
+//     .then(() => console.log("Connected to DB"))
+//     .catch((err) => console.error("DB connection error:", err));
+// };
+
+// module.exports = connectDB;
+
 const mongoose = require("mongoose");
 
 const connectDB = async () => {
+  // Use environment variable or default URL
+  const mongoURL =
+    process.env.MONGODB_URL || "mongodb://mongodb:27017/taskmanager";
+
   await mongoose
-    .connect(process.env.MONGODB_URL)
+    .connect(mongoURL)
     .then(() => console.log("Connected to DB"))
     .catch((err) => console.error("DB connection error:", err));
 };
